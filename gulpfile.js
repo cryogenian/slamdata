b9c89ea8465e5dc53d38e3202866af0b6350ca31
Maxim Zimaliev <zimaliev@yandex.ru>
2015-04-07 00:19:23 +0300

    Squashed commit of the following:
    commit f3dd9639ab2999ed490f33d3246a0d0758642c91
    Author: Maxim Zimaliev <zimaliev@yandex.ru>
    Date:   Tue Apr 7 00:18:27 2015 +0300
    
        removed unneeded file
    
    commit ecc6bd3a74e425154ce43a7e56e6eae1268137b0
    Author: Maxim Zimaliev <zimaliev@yandex.ru>
    Date:   Tue Apr 7 00:15:37 2015 +0300
    
        mandragora bucket updated, notebook and file entries
    
    commit d0f6fea78d94a944316661ba742202c888ec9733
    Merge: 7a4154e 4685b31
    Author: Maxim Zimaliev <zimaliev@yandex.ru>
    Date:   Mon Apr 6 23:12:14 2015 +0300
    
        Merge branch 'master' of github.com:slamdata/slamdata
    
    commit 7a4154e5c91519ace87404dd6efe44f1adc107c2
    Merge: e53e710 3a189d3
    Author: Maxim Zimaliev <zimaliev@yandex.ru>
    Date:   Mon Apr 6 18:45:03 2015 +0300
    
        wip
    
    commit e53e7105c415fe695f97eee7066bcdcba493a18d
    Author: Maxim Zimaliev <zimaliev@yandex.ru>
    Date:   Mon Apr 6 18:42:00 2015 +0300
    
        gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
-var mandragora = require("mandragora-bucket");
-
-mandragora.define({
+var mandragora = require("mandragora-bucket/index.js");
+var gulp = require("gulp");
+mandragora(gulp, {
     paths: {
         bower: [
             "bower_components/purescript-*/src/**/*.purs",
             "bower_components/purescript-*/purescript-*/src/**/*.purs"
-        ]
-    }
+        ],
+        test: ["test/**/*.purs"],
+        src: ["src/**/*.purs"]
+    },
+    tmpDir: "tmp",
+    entries: {
+        "Entries.File": {
+            "name": "file",
+            "dir": "public"
+        },
+        "Entries.Notebook": {
+            "name": "notebook",
+            "dir": "public"
+        }
+    },
+    docs: "MODULES.md",
+    tmpDir: "dist"
 });
+
+
+gulp.task("default", ["watch-main"]);
