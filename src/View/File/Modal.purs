0d68869beee3449ad63c73371e10ee46abc4ebdc
Gary Burgess <gary.burgess@gmail.com>
2015-04-21 16:48:03 +0100

    Remove duplicated code

--- src/View/File/Modal.purs
+++ src/View/File/Modal.purs
@@ -4,13 +4,14 @@ import Control.Alternative (Alternative)
 import Control.Functor (($>))
 import Data.Maybe (Maybe(..), maybe)
 import Model.File
+import View.File.Modal.Common
+import View.File.Modal.ShareDialog
 import qualified Halogen.HTML as H
 import qualified Halogen.HTML.Attributes as A
 import qualified Halogen.HTML.Events as E
 import qualified Halogen.HTML.Events.Handler as E
 import qualified Halogen.HTML.Events.Monad as E
 import qualified Halogen.Themes.Bootstrap3 as B
-import View.File.Modal.ShareDialog
 
 modal :: forall p m. (Alternative m) => (Request -> m Input) -> State -> H.HTML p (m Input)
 modal handler state =
@@ -26,21 +27,6 @@ modal handler state =
         ]
 
     where
-
-    h4 :: forall i. String -> [H.HTML p i]
-    h4 str = [ H.h4_ [H.text str] ]
-
-    section :: forall i. [A.ClassName] -> [H.HTML p i] -> H.HTML p i
-    section clss = H.div [A.classes clss]
-
-    header :: forall i. [H.HTML p i] -> H.HTML p i
-    header = section [B.modalHeader]
-
-    body :: forall i. [H.HTML p i] -> H.HTML p i
-    body = section [B.modalBody]
-
-    footer :: forall i. [H.HTML p i] -> H.HTML p i
-    footer = section [B.modalFooter]
 
     modalContent :: Maybe DialogResume -> [H.HTML p (m Input)]
     modalContent Nothing = []
