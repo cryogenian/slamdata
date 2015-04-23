9b421a8cb632ba4363c14b1e481f941251bbf49a
Gary Burgess <gary.burgess@gmail.com>
2015-04-23 11:51:07 +0100

    Add Inject1 typeclass

---
+++ src/Control/Inject1.purs
@@ -0,0 +1,20 @@
+module Control.Inject1 where
+
+import Data.Either (Either(..), either)
+import Data.Maybe (Maybe(..))
+
+class Inject1 a b where
+  inj :: a -> b
+  prj :: b -> Maybe a
+
+instance inject1Reflexive :: Inject1 a a where
+  inj = id
+  prj = Just
+
+instance inject1Left :: Inject1 a (Either a b) where
+  inj = Left
+  prj = either Just (const Nothing)
+
+instance inject1Right :: (Inject1 a b) => Inject1 a (Either c b) where
+  inj = Right <<< inj
+  prj = either (const Nothing) prj
