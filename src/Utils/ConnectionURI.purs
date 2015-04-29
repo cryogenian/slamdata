d1a40d9728f07ecaf109f800994be35b10052e6c
Gary Burgess <gary.burgess@gmail.com>
2015-04-29 22:50:34 +0100

    Connection URI parsing

---
+++ src/Utils/ConnectionURI.purs
@@ -0,0 +1,83 @@
+module Utils.ConnectionURI
+  ( URIParams()
+  , CredentialParams()
+  , HostParams()
+  , PropParams()
+  , parse
+  ) where
+
+import Control.Alt ((<|>))
+import Control.Apply ((*>))
+import Data.Array (null)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..), maybe, fromMaybe)
+import Data.String (joinWith, charAt, fromChar)
+import Text.Parsing.StringParser (Parser(), ParseError(), runParser)
+import Text.Parsing.StringParser.Combinators (many1, sepBy, sepBy1, optionMaybe, manyTill, lookAhead)
+import Text.Parsing.StringParser.String (string, anyChar, anyDigit, eof)
+
+type URIParams =
+  { name :: Maybe String
+  , credentials :: Maybe CredentialParams
+  , hosts :: Array HostParams
+  , props :: Array PropParams
+  }
+
+type CredentialParams = { user :: String, password :: String }
+type HostParams = { host :: String, port :: Maybe String }
+type PropParams = { name :: String, value :: String }
+
+toURI :: URIParams -> String
+toURI params = "mongodb://"
+  ++ maybe "" credentialsToURI params.credentials
+  ++ (joinWith "," $ hostToURI <$> params.hosts)
+  ++ "/" ++ (fromMaybe "" params.name)
+  ++ if null params.props
+     then ""
+     else "?" ++ (joinWith "&" $ propToURI <$> params.props)
+  where
+  credentialsToURI :: CredentialParams -> String
+  credentialsToURI cred = cred.user ++ ":" ++ cred.password ++ "@"
+  hostToURI :: HostParams -> String
+  hostToURI host = host.host ++ (maybe "" (":" ++) host.port)
+  propToURI :: PropParams -> String
+  propToURI prop = prop.name ++ "=" ++ prop.value
+
+parse :: String -> Either ParseError URIParams
+parse = runParser parseURI
+
+parseURI :: Parser URIParams
+parseURI = do
+  string "mongodb://"
+  credentials <- optionMaybe parseCredentials
+  hosts <- sepBy1 parseHost (string ",")
+  name <- optionMaybe parseDatabaseName
+  options <- sepBy parseProp parsePropSep
+  eof
+  return { name: name
+         , credentials: credentials
+         , hosts: hosts
+         , props: options
+         }
+
+parseCredentials :: Parser { user :: String, password :: String }
+parseCredentials = { user: _, password: _ }
+  <$> (joinWith "" <$> manyTill anyChar (string ":"))
+  <*> (joinWith "" <$> manyTill anyChar (string "@"))
+
+parseHost :: Parser { host :: String, port :: Maybe String }
+parseHost = { host: _, port: _ }
+  <$> (joinWith "" <$> manyTill anyChar (string ":" <|> string "/"))
+  <*> (optionMaybe $ joinWith "" <$> many1 anyDigit)
+
+parseDatabaseName :: Parser String
+parseDatabaseName =
+  joinWith "" <$> manyTill anyChar ((string "?" *> pure unit) <|> eof)
+
+parseProp :: Parser { name :: String, value :: String }
+parseProp = { name: _, value: _ }
+  <$> (joinWith "" <$> manyTill anyChar (string "="))
+  <*> (joinWith "" <$> manyTill anyChar (lookAhead parsePropSep <|> eof))
+
+parsePropSep :: Parser Unit
+parsePropSep = (string "&" <|> string ";") *> pure unit
--- src/View/File/Modal/MountDialog.purs
+++ src/View/File/Modal/MountDialog.purs
@@ -1,6 +1,11 @@
 module View.File.Modal.MountDialog (mountDialog) where
 
 import Data.Inject1 (inj)
+import Control.Apply ((*>))
+import Control.Functor (($>))
+import Control.Plus (empty)
+import Control.Monad.Aff.Class (liftAff)
+import Control.Monad.Eff.Class (liftEff)
 import Controller.File (selectThis)
 import Data.Array ((..), length, zipWith)
 import Data.Int (toNumber)
@@ -10,7 +15,8 @@ import Input.File.Mount (MountInput(..))
 import Optic.Core ((.~), (^.))
 import Optic.Index (ix)
 import Optic.Index.Types (TraversalP())
-import Utils.Halide (readonly)
+import Utils.Halide (readonly, onPaste)
+import Utils (select, clearValue)
 import View.Common (glyph)
 import View.File.Common (I())
 import View.File.Modal.Common (header, h4, body, footer)
@@ -18,7 +24,9 @@ import qualified Halogen.HTML as H
 import qualified Halogen.HTML.Attributes as A
 import qualified Halogen.HTML.Events as E
 import qualified Halogen.HTML.Events.Forms as E
+import qualified Halogen.HTML.Events.Handler as E
 import qualified Halogen.HTML.Events.Monad as E
+import qualified Halogen.HTML.Events.Types as ET
 import qualified Halogen.Themes.Bootstrap3 as B
 import qualified Model.File.Dialog.Mount as M
 
@@ -27,7 +35,8 @@ mountDialog state =
   [ header $ h4 "Mount"
   , body [ H.form [ A.class_ (A.className "dialog-mount") ]
                   $ (if state.new then [fldName state] else [])
-                 ++ [ selScheme state
+                 ++ [ fldConnectionURI state
+                    , selScheme state
                     , hosts state
                     , fldUser state
                     , fldPass state
@@ -44,6 +53,43 @@ fldName state =
   H.div [ A.class_ B.formGroup ]
         [ label "Name" [ input state M.name [] ] ]
 
+fldConnectionURI :: forall p e. M.MountDialogRec -> H.HTML p (I e)
+fldConnectionURI state =
+  H.div [ A.classes [B.formGroup, A.className "mount-uri"] ]
+        [ label "Connection URI"
+                [ H.input [ A.class_ B.formControl
+                          , A.value state.connectionURI
+                          , E.onKeyDown clearText
+                          , E.onKeyPress handleKeyInput
+                          , onPaste selectText
+                          , E.onInput (E.input \value -> inj $ UpdateConnectionURI value)
+                          ]
+                          []
+                ]
+        ]
+  where
+
+  -- Delete the entire connection URI contents when backspace or delete is used.
+  clearText :: ET.Event ET.KeyboardEvent -> E.EventHandler (I e)
+  clearText e =
+    if (e.keyCode == 8 || e.keyCode == 46)
+    then E.preventDefault $> (liftEff (clearValue e.target) *> pure (inj $ UpdateConnectionURI ""))
+    else pure empty
+
+  -- Ignore key inputs aside from Ctrl+V or Meta+V. When any other keypress is
+  -- detected select the current contents instead.
+  handleKeyInput :: ET.Event ET.KeyboardEvent -> E.EventHandler (I e)
+  handleKeyInput e =
+    if (e.ctrlKey || e.metaKey) && e.charCode == 118
+    then pure empty
+    else E.preventDefault $> (liftEff (select e.target) *> empty)
+
+  -- In Chrome, this is used to prevent multiple values being pasted in the
+  -- field - once pasted, the value is selected so that the new value replaces
+  -- it.
+  selectText :: forall a. ET.Event a -> E.EventHandler (I e)
+  selectText e = pure $ liftEff (select e.target) *> empty
+
 selScheme :: forall p e. M.MountDialogRec -> H.HTML p (I e)
 selScheme state =
   H.div [ A.class_ B.formGroup ]
@@ -55,7 +101,7 @@ selScheme state =
 
 hosts :: forall p e. M.MountDialogRec -> H.HTML p (I e)
 hosts state =
-  H.div [ A.classes [B.formGroup] ]
+  H.div [ A.classes [B.formGroup, A.className "mount-host-list"] ]
         $ (host state) <$> 0 .. (length state.hosts - 1)
 
 host :: forall p e. M.MountDialogRec -> Number -> H.HTML p (I e)
