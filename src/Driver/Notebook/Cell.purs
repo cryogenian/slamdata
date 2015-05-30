de8cf1bf07d767e022de68ef4d59b6d2adb84189
Brian McKenna <brian@brianmckenna.org>
2015-05-30 12:27:35 -0600

    Fix evaluation of non-Ace components

--- src/Driver/Notebook/Cell.purs
+++ src/Driver/Notebook/Cell.purs
@@ -8,15 +8,24 @@ import EffectTypes (NotebookAppEff(), NotebookComponentEff())
 import Halogen (Driver())
 import Halogen.HTML.Events.Monad (runEvent)
 import Input.Notebook (Input(..))
-import Model.Notebook.Cell (_cellId)
+import Model.Notebook.Cell (Cell(), CellContent(..), _cellId, _content)
 import Optic.Core ((^.))
 
 driveEvent :: forall e. Driver Input (NotebookComponentEff e) -> I e -> Eff (NotebookAppEff e) Unit
 driveEvent = runEvent (const $ return unit)
 
 driveCellContent :: forall eff. Input -> Driver Input (NotebookComponentEff eff) -> Eff (NotebookAppEff eff) Unit
-driveCellContent (ReceiveCellContent cell) driver = do
+driveCellContent (RequestCellContent cell) driver =
+  case cell ^. _content of
+    Search _ -> runWith cell driver
+    Explore _ -> runWith cell driver
+    Visualize _ -> runWith cell driver
+    _ -> return unit
+driveCellContent (ReceiveCellContent cell) driver = runWith cell driver
+driveCellContent _ _ = return unit
+
+runWith :: forall eff. Cell -> Driver Input (NotebookComponentEff eff) -> Eff (NotebookAppEff eff) Unit
+runWith cell driver = do
   d <- now
   driver $ StartRunCell (cell ^. _cellId) d
   driveEvent driver $ runCell cell
-driveCellContent _ _ = return unit
