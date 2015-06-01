349c249d4c7391ff94f3ff8da482b462bc99ee27
Maxim Zimaliev <zimaliev@yandex.ru>
2015-06-01 14:00:27 +0300

    correctly splitted pie charts

--- src/Controller/Notebook/Cell/Viz/Pie.purs
+++ src/Controller/Notebook/Cell/Viz/Pie.purs
@@ -34,13 +34,16 @@ import Controller.Notebook.Cell.Viz.Bar (extractClean, AggregatedAccum())
 import Data.Bifunctor (lmap)
 import Data.String (split)
 import Math (floor)
+import Data.Bifunctor (lmap)
+import Data.String (split)
 
 rowLength :: Number
 rowLength = 4
 
 mkSeries :: AggregatedAccum -> [Series]
 mkSeries acc =
-  concat (zipWith (rows $ length srcs) (range 0 $ length srcs) srcs)
+  serie <$> (toList group)
+--  concat (zipWith (rows $ length items) (range 0 $ length items) items)
   where
   rows :: Number -> Number -> [[Tuple Key ItemData]] -> [Series]
   rows count ix lst =
@@ -90,9 +93,6 @@ mkSeries acc =
         c = Just $ Tuple (Percent x) (Percent y)
     in Tuple r c
 
-  itemDatas :: [Tuple Key ItemData]
-  itemDatas = mkItemData <$> (toList acc)
-
   mkItemData :: Tuple Key Number -> Tuple Key ItemData
   mkItemData (Tuple k n) =
     Tuple k 
@@ -103,17 +103,56 @@ mkSeries acc =
                         then ""
                         else " " <> name)
 
-  src :: [[Tuple Key ItemData]]
-  src = groupBy (\(Tuple a _) (Tuple b _) -> keyMbSeries1 a == keyMbSeries1 b) itemDatas
 
-  srcs :: [[[Tuple Key ItemData]]]
-  srcs = (groupBy (\(Tuple a _) (Tuple b _) -> keyMbSeries2 a == keyMbSeries2 b)) <$> src
+  nameMap :: [Tuple Key Number] -> Map String [Number]
+  nameMap = named' >>> named''
 
+  named :: [Tuple Key Number] -> String -> Map String Number
+  named lst cat =
+    fromList
+    ((\x -> lmap keyName x) <$>
+     (filter (\(Tuple k _) -> keyCategory k == cat) lst))
+
+  named' :: [Tuple Key Number] -> [Map String Number]
+  named' lst = named lst <$> catVals
+
+  catVals :: [String]
+  catVals = nub $ keyCategory <$> ks
+
+  ks :: [Key]
+  ks = keys acc
+
+  named'' :: [Map String Number] -> Map String [Number]
+  named'' m =
+    reverse <$> (foldl foldFn empty (toList <$> m))
+
+  foldFn :: Map String [Number] -> [Tuple String Number] -> Map String [Number]
+  foldFn m tpls =
+    foldl (\m (Tuple k n) -> alter (alterNamed n) k m) m tpls
+
+  alterNamed :: Number -> Maybe [Number] -> Maybe [Number]
+  alterNamed n ns =
+    Just $ (n:(fromMaybe [] ns))
+
+  group :: Map String [Number]
+  group = nameMap $ toList acc
+
+  dat :: String -> Number -> ItemData 
+  dat str n = (Dat $ (dataDefault $ Simple n) {name = Just $ str})
+
+  serie :: Tuple String [Number] -> Series
+  serie (Tuple k nums) =
+    PieSeries { common: universalSeriesDefault
+              , pieSeries: pieSeriesDefault { "data" = Just $ (dat k) <$> nums}
+              } 
+  
   mkSerie :: [Tuple Key ItemData] -> Series
   mkSerie lst =
     PieSeries { common: universalSeriesDefault
               , pieSeries: pieSeriesDefault { "data" = Just $ snd <$> lst }
               }
+
+import Debug.Foreign
 
 mkPie :: forall e. VizRec -> _ -> Option
 mkPie r conf =
