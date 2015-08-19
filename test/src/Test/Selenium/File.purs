377f48b0c7fbad2a96d6df7c02f40257d5efaa99
Jonathan Sterling <jon@jonmsterling.com>
2015-08-19 10:07:55 -0700

    [tests] Test mounting a database with a bad port
    It was necessary to upgrade the SlamEngine jar used in the tests to at
    least 2.1.7, which changed the mongo timeout to 5s.
    
    In addition, one change in SlamEngine behavior is that it seems to now
    be required to specify the name of the database in the connection URI
    (mongo://...:12345/testdb). I've updated the test code accordingly.
    
    The added test tries to mount a database with an incorrect port, and
    checks that the correct error is presented in the correct way.

--- test/src/Test/Selenium/File.purs
+++ test/src/Test/Selenium/File.purs
@@ -100,10 +100,20 @@ getUploadedItem =
   findUploadedItem
     >>= maybe (errorMsg "File has not been uploaded") pure
 
+type MountConfigR =
+  { host :: String
+  , port :: Int
+  }
 
-mountDatabase :: Check Unit
-mountDatabase = do
-  sectionMsg "MOUNT TEST DATABASE"
+mountConfigFromConfig :: Check MountConfigR
+mountConfigFromConfig = do
+  config <- getConfig
+  pure { host : config.mongodb.host
+       , port : config.mongodb.port
+       }
+
+mountDatabaseWithMountConfig :: MountConfigR -> Check Unit
+mountDatabaseWithMountConfig mountConfig = do
   home
   mountButton <- getMountDatabaseButton
   actions $ leftClick mountButton
@@ -113,7 +123,7 @@ mountDatabase = do
   nameField <- getElementByCss config.configureMount.nameField "no mount name field"
   saveButton <- getElementByCss config.configureMount.saveButton "no save button"
 
-  let connectionUri = "mongodb://" ++ config.mongodb.host ++ ":" ++ show config.mongodb.port ++ "/"
+  let connectionUri = "mongodb://" ++ mountConfig.host ++ ":" ++ show mountConfig.port ++ "/" ++ config.database.name
   actions $ do
     -- a strange hack follows to get the uri onto the clipboard, since the uri
     -- field cannot be edited except by pasting.
@@ -128,8 +138,6 @@ mountDatabase = do
     sendPaste
     leftClick saveButton
 
-  waitCheck mountShown config.selenium.waitTime
-
   where
   getMountDatabaseButton :: Check Element
   getMountDatabaseButton = do
@@ -137,10 +145,47 @@ mountDatabase = do
     toolbarButton config.toolbar.mountDatabase
       >>= maybe (errorMsg "No mount database button") pure
 
-  mountShown :: Check Boolean
-  mountShown = checker $ do
-    config <- getConfig
-    isJust <$> findItem config.mount.name
+goodMountDatabase :: Check Unit
+goodMountDatabase = do
+  sectionMsg "MOUNT TEST DATABASE"
+  mountConfigFromConfig
+    >>= mountDatabaseWithMountConfig
+
+  config <- getConfig
+  waitCheck mountShown config.selenium.waitTime
+
+  where
+    mountShown :: Check Boolean
+    mountShown = checker $ do
+      config <- getConfig
+      isJust <$> findItem config.mount.name
+
+badMountDatabase :: Check Unit
+badMountDatabase = do
+  sectionMsg "BAD MOUNT TEST DATABASE"
+  config <- getConfig
+
+  badMountConfig
+    >>= mountDatabaseWithMountConfig
+
+  waitCheck (checker expectWarningMessage) 8000
+
+  cancelButton <- getElementByCss config.configureMount.cancelButton "no cancel button"
+  actions $ leftClick cancelButton
+  waitCheck modalDismissed config.selenium.waitTime
+
+  where
+    expectWarningMessage :: Check Boolean
+    expectWarningMessage = do
+      config <- getConfig
+      getElementByCss config.configureMount.warningBox "no warning box"
+        >>= innerHtml
+        <#> R.test (R.regex "Invalid server and / or port specified" R.noFlags)
+
+    badMountConfig :: Check MountConfigR
+    badMountConfig = do
+      mountConfig <- mountConfigFromConfig
+      pure $ mountConfig { port = mountConfig.port - 1 }
 
 unmountDatabase :: Check Unit
 unmountDatabase = do
@@ -691,9 +736,10 @@ checkTitle = do
 test :: Check Unit
 test = do
   home
-  mountDatabase
+  badMountDatabase
+  goodMountDatabase
   unmountDatabase
-  mountDatabase
+  goodMountDatabase
   checkMountedDatabase
   checkConfigureMount
   checkItemToolbar
