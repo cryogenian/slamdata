3185b2e9d169194ac2524d7b429d53b5fe6bafde
Gary Burgess <gary.burgess@gmail.com>
2015-11-06 15:52:39 +0000

    Remove unused stuff in Def

--- wip/src/Notebook/Cell/Component/Def.purs
+++ wip/src/Notebook/Cell/Component/Def.purs
@@ -1,7 +1,5 @@
 module Notebook.Cell.Component.Def
-  ( CellPart(..)
-  , Def()
-  , EditorDef()
+  ( EditorDef()
   , ResultsDef()
   , CellProps()
   ) where
@@ -20,11 +18,6 @@ import Notebook.Cell.Component.Query (AnyCellQuery())
 import Notebook.Cell.Component.State (AnyCellState())
 import Notebook.Common (Slam())
 
-data CellPart = EditorPart | ResultsPart
-derive instance genericCellPart :: Generic CellPart
-instance eqAnyCellSlot :: Eq CellPart where eq = gEq
-instance ordAnyCellSlot :: Ord CellPart where compare = gCompare
-
 type CellProps s f r =
   ( component :: Component s (Coproduct CellEvalQuery f) Slam
   , initialState :: s
@@ -40,16 +33,3 @@ type EditorDef s f =
   }
 
 type ResultsDef s f = Object (CellProps s f ())
-
-type Def se fe sr fr =
-  { name :: String
-  , glyph :: H.ClassName
-  , editor :: Component se (Coproduct CellEvalQuery fe) Slam
-  , editorState :: se
-  , _StateE :: APrismP AnyCellState se
-  , _QueryE :: forall a. APrismP (AnyCellQuery a) (fe a)
-  , results :: Component sr (Coproduct CellEvalQuery fr) Slam
-  , resultsState :: sr
-  , _StateR :: APrismP AnyCellState sr
-  , _QueryR :: forall a. APrismP (AnyCellQuery a) (fr a)
-  }
