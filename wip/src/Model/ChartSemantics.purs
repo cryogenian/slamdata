7274dd53805e46c26a4a203832c2c6770f3fe3e0
Maxim Zimaliev <zimaliev@gmail.com>
2015-12-12 00:37:28 +0300

    optimised

--- wip/src/Model/ChartSemantics.purs
+++ wip/src/Model/ChartSemantics.purs
@@ -19,19 +19,23 @@ module Model.ChartSemantics where
 import Prelude
 
 import Control.Alt ((<|>))
+import Control.Apply ((*>))
+import Control.Monad.Eff (Eff())
+import Control.Monad.ST (STRef(), ST(), newSTRef, modifySTRef, readSTRef, pureST)
 import Control.MonadPlus (guard)
 import Data.Argonaut
   ( runJsonPrim, toPrims, JsonPrim(), Json(), JArray(), JCursor()
   , DecodeJson, EncodeJson, decodeJson, jsonEmptyObject, (:=), (.?), (~>))
 import Data.Array as A
+import Data.Array.ST
 import Data.Either (Either(..))
-import Data.Foldable (foldl)
+import Data.Foldable (foldl, foldMap)
 import Data.List (List(..), reverse, catMaybes, toList)
 import Data.List as L
 import Data.Map (Map(), keys, update, lookup, fromList)
 import Data.Maybe (Maybe(..), isJust)
 import Data.String (take)
-import Data.String.Regex (noFlags, regex, match, test)
+import Data.String.Regex (Regex(), noFlags, regex, match, test)
 import Data.Traversable (traverse)
 import Data.Tuple (Tuple(..))
 import Utils (stringToNumber, stringToInt)
@@ -82,14 +86,7 @@ semanticsToNumber (Money v _) = pure v
 semanticsToNumber (Percent v) = pure v
 semanticsToNumber _ = Nothing
 
--- | This values usually represents `Nothing` in mongodb
-nothings :: Array Semantics
-nothings = map Category [ "undefined"
-                        , "null"
-                        , "NA"
-                        , "N/A"
-                        , ""
-                        ]
+
 
 -- | Used as accumulator in `checkPredicate` only
 type CheckAccum =
@@ -98,47 +95,65 @@ type CheckAccum =
   , filtered :: List (Maybe Semantics)
   }
 
+emptyAccum :: CheckAccum
+emptyAccum =
+  { correct: zero
+  , incorrect: zero
+  , filtered: Nil
+  }
+
 -- | This function checks values in list of `Maybe Semantics` and
 -- | returns a list with values that satisfies predicate wrapped in `Just`
 -- | or (if there more incorrect values than correct) it returns `Nothing`
 checkPredicate
   :: (Semantics -> Boolean) -> List (Maybe Semantics)
   -> Maybe (List (Maybe Semantics))
-checkPredicate p lst =
-  if result.correct > result.incorrect
-  then pure $ reverse result.filtered
-  else Nothing
-  where
-  result :: CheckAccum
-  result = foldl (foldFn p) emptyAccum lst
+checkPredicate p lst = pureST do
+  corrects <- newSTRef 0
+  incorrects <- newSTRef 0
+  filtered <- newSTRef Nil
+  foldl
+    (\_ a -> checkPredicateTraverseFn p corrects incorrects filtered a)
+    (pure unit) lst
+  c <- readSTRef corrects
+  ic <- readSTRef incorrects
+  if c > ic
+    then do
+    f <- readSTRef filtered
+    pure $ Just f
+    else
+    pure Nothing
 
-  emptyAccum :: CheckAccum
-  emptyAccum =
-    { correct: zero
-    , incorrect: zero
-    , filtered: Nil
-    }
+checkPredicateTraverseFn
+  :: forall h
+   . (Semantics -> Boolean)
+  -> STRef h Int -> STRef h Int -> STRef h (List (Maybe Semantics))
+  -> Maybe Semantics -> Eff (st :: ST h) Unit
+checkPredicateTraverseFn _ corrects incorrects filtered Nothing = do
+  modifySTRef filtered (Cons Nothing)
+  pure unit
+checkPredicateTraverseFn p corrects incorrects filtered (Just c)
+  | isUsedAsNothing c = do
+    modifySTRef filtered $ Cons Nothing
+    pure unit
 
-  foldFn :: (Semantics -> Boolean) -> CheckAccum -> Maybe Semantics -> CheckAccum
-  -- There is no value. Just put it to acc don't touch counters
-  foldFn _ accum Nothing = accum { filtered = Cons Nothing accum.filtered }
-  foldFn predicate accum (Just c)
-    -- It means nothing. Put Nothing to accum and don't touch counters
-    | isUsedAsNothing c =
-        accum { filtered = Cons Nothing accum.filtered}
-    -- It's correct. Increase correct counter, put value to accum
-    | predicate c =
-        accum { filtered = Cons (Just c) accum.filtered
-              , correct = accum.correct + one
-              }
-    -- It's incorrect. Increase incorrect counter, put nothing to accum
-    | otherwise =
-        accum { filtered = Cons Nothing accum.filtered
-              , incorrect = accum.incorrect + one
-              }
-  isUsedAsNothing :: Semantics -> Boolean
-  isUsedAsNothing c = isJust $ A.elemIndex c nothings
+  | p c = do
+    modifySTRef filtered $ Cons $ Just c
+    modifySTRef corrects (+ 1)
+    pure unit
 
+  | otherwise = do
+    modifySTRef filtered $ Cons Nothing
+    modifySTRef incorrects (+ 1)
+    pure unit
+
+isUsedAsNothing :: Semantics -> Boolean
+isUsedAsNothing (Category "undefined") = true
+isUsedAsNothing (Category "null") = true
+isUsedAsNothing (Category "NA") = true
+isUsedAsNothing (Category "N/A") = true
+isUsedAsNothing (Category "") = true
+isUsedAsNothing _ = false
 
 instance semanthicShow :: Show Semantics where
   show (Value v) = "(Value " <> show v <> ")"
@@ -204,18 +219,26 @@ analyzeNumber s = do
   guard $ show num == s || show int == s
   pure $ Value num
 
+percentRegex :: Regex
+percentRegex = regex """^(-?\d+(\.\d+)?)\%$""" noFlags
+
 analyzePercent :: String -> Maybe Semantics
 analyzePercent input = do
-  matches <- match rgx input
+  matches <- match percentRegex input
   maybeMatch <- matches A.!! 1
   num <- maybeMatch >>= stringToNumber
   pure $ Percent num
+
+moneyRegex :: Regex
+moneyRegex = regex rgxStr noFlags
   where
-  rgx = regex """^(-?\d+(\.\d+)?)\%$""" noFlags
+  rgxStr = "^" <> curSymbols <> """?(([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?)$"""
+  curSymbols :: String
+  curSymbols = """[\$\u20A0-\u20CF\u00A2\u00A3\u00A4\u00A5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]"""
 
 analyzeMoney :: String -> Maybe Semantics
 analyzeMoney str = do
-  matches <- match rgx str
+  matches <- match moneyRegex str
   maybeMatch <- matches A.!! 1
   num <- maybeMatch >>= stringToNumber
   currencySymbol <- let fstSymbol = take 1 str
@@ -223,38 +246,44 @@ analyzeMoney str = do
                        then Nothing
                        else pure fstSymbol
   pure $ Money num currencySymbol
+
+dateRegex :: Regex
+dateRegex = regex rgxStr noFlags
   where
-  rgx = regex rgxStr noFlags
-  rgxStr = "^" <> curSymbols <> """?(([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?)$"""
-  curSymbols :: String
-  curSymbols = """[\$\u20A0-\u20CF\u00A2\u00A3\u00A4\u00A5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]"""
+  rgxStr = "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[0-1]|0[1-9]|[1-2][0-9]) (2[0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])(\\.[0-9]+)?(Z|[+-](?:2[0-3]|[0-1][0-9]):[0-5][0-9])?$"
 
 analyzeDate :: String -> Maybe Semantics
 analyzeDate str = do
-  guard $ test rgx str
+  guard $ test dateRegex str
   pure $ Time str
-  where
-  rgxStr = "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[0-1]|0[1-9]|[1-2][0-9]) (2[0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])(\\.[0-9]+)?(Z|[+-](?:2[0-3]|[0-1][0-9]):[0-5][0-9])?$"
-  rgx = regex rgxStr noFlags
 
 jsonToSemantics :: Json -> Map JCursor Semantics
 jsonToSemantics j = fromList $ catMaybes $ map (traverse analyze) $ toPrims j
 
 jarrayToSemantics :: JArray -> Map JCursor (List (Maybe Semantics))
-jarrayToSemantics arr = step initial mapLst
+jarrayToSemantics arr = foldl foldFn initial mapArr
   where
-  mapArr = map jsonToSemantics $ A.reverse arr
-  mapLst = toList mapArr
-  ks = toList $ A.nub $ A.concat $ map (L.fromList <<< keys) mapArr
+  mapArr :: Array (Map JCursor Semantics)
+  mapArr = map jsonToSemantics arr
+
+  initial :: Map JCursor (List (Maybe Semantics))
   initial = fromList $ map (flip Tuple Nil) ks
 
-  step acc Nil = acc
-  step acc (Cons m lst) =
-    step (insertOne acc m ks) lst
+  ks :: List JCursor
+  ks = L.nub $ foldMap keys mapArr
 
-  insertOne acc m Nil = acc
-  insertOne acc m (Cons k ks') =
-    insertOne (update (pure <<< Cons (lookup k m)) k acc) m ks'
+  foldFn
+    :: Map JCursor (List (Maybe Semantics))
+    -> Map JCursor Semantics
+    -> Map JCursor (List (Maybe Semantics))
+  foldFn acc m = foldl (insertOne m) acc ks
+
+  insertOne
+    :: Map JCursor Semantics
+    -> Map JCursor (List (Maybe Semantics))
+    -> JCursor
+    -> Map JCursor (List (Maybe Semantics))
+  insertOne m acc k = update (pure <<< Cons (lookup k m)) k acc
 
 checkValues :: List (Maybe Semantics) -> Maybe (List (Maybe Semantics))
 checkValues = checkPredicate isValue
