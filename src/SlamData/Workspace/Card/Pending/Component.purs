1243f48c1a549c0119eee7dab1ea163d945ffacb
Gary Burgess <gary.burgess@gmail.com>
2016-07-11 21:44:25 +0100

    Update pending card style

--- src/SlamData/Workspace/Card/Pending/Component.purs
+++ src/SlamData/Workspace/Card/Pending/Component.purs
@@ -17,8 +17,8 @@ limitations under the License.
 module SlamData.Workspace.Card.Pending.Component where
 
 import SlamData.Prelude
-import SlamData.Effects (Slam)
 
+import SlamData.Effects (Slam)
 import SlamData.Workspace.Card.CardType as CT
 import SlamData.Workspace.Card.Component as CC
 import SlamData.Workspace.Card.Model as Card
@@ -26,9 +26,7 @@ import SlamData.Workspace.Card.Pending.Component.Query as PCQ
 import SlamData.Workspace.Card.Pending.Component.State as PCS
 
 import Halogen as H
-import Halogen.Themes.Bootstrap3 as B
 import Halogen.HTML.Indexed as HH
-import Halogen.HTML.Properties.Indexed as HP
 
 type DSL = H.ComponentDSL PCS.State PCQ.QueryP Slam
 type HTML = H.ComponentHTML PCQ.QueryP
@@ -43,32 +41,29 @@ comp =
     , _Query: CC.makeQueryPrism CC._PendingQuery
     }
 
-render
-  ∷ PCS.State
-  → HTML
+render ∷ PCS.State → HTML
 render st =
-  HH.div [ HP.classes [ B.alert, B.alertInfo ] ]
-    [ HH.img [ HP.src "img/blue-spin.svg" ]
-    , HH.text st.message
+  HH.div_
+    [ HH.i_ []
+    , HH.span_ [ HH.text st.message ]
     ]
 
 eval ∷ PCQ.QueryP ~> DSL
 eval = coproduct cardEval PCQ.initiality
 
 cardEval ∷ CC.CardEvalQuery ~> DSL
-cardEval q =
-  case q of
-    CC.EvalCard _ _ next → do
-      pure next
-    CC.Activate next →
-      pure next
-    CC.Save k →
-      pure $ k Card.PendingCard
-    CC.Load _ next →
-      pure next
-    CC.SetDimensions _ next →
-      pure next
-    CC.ModelUpdated _ next →
-      pure next
-    CC.ZoomIn next →
-      pure next
+cardEval = case _ of
+  CC.EvalCard _ _ next → do
+    pure next
+  CC.Activate next →
+    pure next
+  CC.Save k →
+    pure $ k Card.PendingCard
+  CC.Load _ next →
+    pure next
+  CC.SetDimensions _ next →
+    pure next
+  CC.ModelUpdated _ next →
+    pure next
+  CC.ZoomIn next →
+    pure next
