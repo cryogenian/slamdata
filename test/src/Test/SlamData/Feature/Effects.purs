3892c53ecc492eb7b375134412a65c7e92bf9375
Nathan Faubion <nathan@n-son.com>
2016-07-29 12:24:12 -0500

    Remove node-mongo

--- test/src/Test/SlamData/Feature/Effects.purs
+++ test/src/Test/SlamData/Feature/Effects.purs
@@ -2,14 +2,12 @@ module Test.SlamData.Feature.Effects where
 
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff.Random (RANDOM)
-import Database.Mongo.Mongo (DB)
 import Node.ChildProcess (CHILD_PROCESS)
 import Test.SlamData.Feature.Env (ENV)
 
 type SlamFeatureEffects e =
   ( cp :: CHILD_PROCESS
   , avar :: AVAR
-  , db :: DB
   , random :: RANDOM
   , env :: ENV
   | e)
