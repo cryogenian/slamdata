2c21ae5e9e6c5f533571b0de05c65b62192819db
Maxim Zimaliev <zimaliev@gmail.com>
2016-08-29 17:48:27 +0300

    everything works but has poor performance

--- src/SlamData/Workspace/Card/Chart/BuildOptions/Graph.purs
+++ src/SlamData/Workspace/Card/Chart/BuildOptions/Graph.purs
@@ -39,8 +39,10 @@ type EdgeItem = String × String
 type GraphItem =
   { size ∷ Maybe Number
   , category ∷ Maybe String
-  , name ∷ Maybe String
   , value ∷ Maybe Number
+  , source ∷ Maybe String
+  , target ∷ Maybe String
+  , name ∷ Maybe String
   }
 
 type GraphData = Array GraphItem × Array EdgeItem
@@ -49,12 +51,12 @@ buildGraphData ∷ M.Map JCursor Axis → GraphR → GraphData
 buildGraphData axesMap r =
   nodes × edges
   where
-  edges ∷ Array EdgeItem
-  edges =
+  rawEdges ∷ Array EdgeItem
+  rawEdges =
     A.nub $ A.catMaybes $ A.zipWith edgeZipper sources targets
 
-  edgeZipper ∷ Maybe String → Maybe String → Maybe (String × String)
-  edgeZipper source target = Tuple <$> source <*> target
+  edgeZipper ∷ Maybe String → Maybe String → Maybe EdgeItem
+  edgeZipper s t = Tuple <$> s <*> t
 
   sources ∷ Array (Maybe String)
   sources =
@@ -71,21 +73,49 @@ buildGraphData axesMap r =
   nodes ∷ Array GraphItem
   nodes =
     A.nubBy (\r1 r2 → r1.name ≡ r2.name)
-      $ map (\(value × size × category × name) →
-              { value
+      $ map (\(source × target × value × size × category) →
+              { source
+              , target
+              , value
               , size
               , category
-              , name: (map (\n → "name:" ⊕ n) name)
+              , name:
+                  (map (\s → "edge source:" ⊕ s) source)
                   ⊕ (map (\c → ":category:" ⊕ c) category)
                   ⊕ (map (\s → ":size:" ⊕ show s) size)
               })
+      $ A.zip (sources ⊕ nothingTail sources)
+      $ A.zip (targets ⊕ nothingTail targets)
       $ A.zip (values ⊕ nothingTail values)
       $ A.zip (sizes ⊕ nothingTail sizes)
-      $ A.zip (categories ⊕ nothingTail categories)
-      $ names ⊕ nothingTail names
+      $ categories ⊕ nothingTail categories
+
+
+  edgeMap ∷ M.Map String (Array String)
+  edgeMap = foldl foldFn M.empty nodes
+
+  foldFn ∷ M.Map String (Array String) → GraphItem → M.Map String (Array String)
+  foldFn acc { source, target, name} = case name of
+    Nothing → acc
+    Just n →
+      maybe id (M.alter (alterFn n)) target
+      $ maybe id (M.alter (alterFn n)) source
+      $ acc
+
+  alterFn ∷ String → Maybe (Array String) → Maybe (Array String)
+  alterFn s Nothing = pure $ A.singleton s
+  alterFn s (Just arr) = pure $ A.cons s arr
 
   maxLength ∷ Int
-  maxLength = fromMaybe zero $ F.maximum [ A.length categories, A.length names, A.length sizes ]
+  maxLength =
+    fromMaybe zero
+    $ F.maximum
+      [ A.length categories
+      , A.length sources
+      , A.length sizes
+      , A.length targets
+      , A.length values
+      ]
 
   nothingTail ∷ ∀ a. Array (Maybe a) → Array (Maybe a)
   nothingTail heads = do
@@ -130,9 +160,16 @@ buildGraphData axesMap r =
   sizes ∷ Array (Maybe Number)
   sizes = map (map relativeSize) values
 
+  edges ∷ Array EdgeItem
+  edges = do
+    (s × t) ← rawEdges
+    ss ← fromMaybe [ ] $ M.lookup s edgeMap
+    tt ← fromMaybe [ ] $ M.lookup t edgeMap
+    pure $ ss × tt
 
-nameRgx ∷ Rgx.Regex
-nameRgx = unsafePartial fromRight $ Rgx.regex "name:(\\w+)" Rgx.noFlags
+
+sourceRgx ∷ Rgx.Regex
+sourceRgx = unsafePartial fromRight $ Rgx.regex "source:(\\w+)" Rgx.noFlags
 
 categoryRgx ∷ Rgx.Regex
 categoryRgx = unsafePartial fromRight $ Rgx.regex "category:(\\w+)" Rgx.noFlags
@@ -146,13 +183,13 @@ buildGraph r records = do
       E.fontSize 12
     E.formatterItem \{name, value} →
       let
-        mbName ∷ Maybe String
-        mbName = join $ Rgx.match nameRgx name >>= flip A.index 1
+        mbSource ∷ Maybe String
+        mbSource = join $ Rgx.match sourceRgx name >>= flip A.index 1
 
         mbCat ∷ Maybe String
         mbCat = join $ Rgx.match categoryRgx name >>= flip A.index 1
       in
-        (foldMap (\n → "name: " ⊕ n) mbName)
+        (foldMap (\s → "source: " ⊕ s) mbSource)
         ⊕ (foldMap (\c → "<br /> category: " ⊕ c) mbCat)
         ⊕ "<br /> value: " ⊕ show value
 
@@ -171,8 +208,8 @@ buildGraph r records = do
       else E.layoutForce
 
     E.force do
-      E.edgeLength 50.0
-      E.repulsion 100.0
+      E.edgeLength 100.0
+      E.repulsion 200.0
       E.gravity 0.2
       E.layoutAnimation true
 
@@ -204,7 +241,7 @@ buildGraph r records = do
   items = for_ (fst graphData) \item → E.addItem do
     for_ (item.category >>= flip A.elemIndex legendNames) E.category
     traverse_ E.symbolSize $ map Int.floor item.size
-    traverse_ E.value item.value
+    E.value $ fromMaybe zero item.value
     traverse_ E.name item.name
     E.itemStyle $ E.normal do
       E.borderWidth 1
