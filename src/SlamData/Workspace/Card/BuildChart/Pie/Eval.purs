cd4b04c87964339ec6450ccff70bf41463678ed5
Maxim Zimaliev <zimaliev@gmail.com>
2016-09-19 23:43:03 +0300

    pie wip

--- src/SlamData/Workspace/Card/BuildChart/Pie/Eval.purs
+++ src/SlamData/Workspace/Card/BuildChart/Pie/Eval.purs
@@ -12,9 +12,11 @@ import Data.Lens ((^?))
 import Data.Lens as Lens
 import Data.Map as M
 import Data.Int as Int
+import Data.Set as Set
 
 import ECharts.Monad (DSL)
 import ECharts.Commands as E
+import ECharts.Types as ET
 import ECharts.Types.Phantom (OptionI)
 import ECharts.Types.Phantom as ETP
 
@@ -59,6 +61,162 @@ eval (Just conf) resource = do
   pure $ Port.ChartInstructions (buildPie conf records) Pie
 
 
+infixr 3 type M.Map as >>
+
+
+type OnePieSeries =
+  { name ∷ Maybe String
+  , x ∷ Maybe Number
+  , y ∷ Maybe Number
+  , radius ∷ Maybe Number
+  , series ∷ Array DonutSeries
+  }
+
+type DonutSeries =
+  { radius ∷ Maybe {start ∷ Number, end ∷ Number}
+  , name ∷ Maybe String
+  , items ∷ String >> Number
+  }
+
+
+buildPieData ∷ PieR → JArray → Array OnePieSeries
+buildPieData r records = series
+  where
+  -- | maybe parallel >> maybe donut >> category name >> values
+  dataMap ∷ Maybe String >> Maybe String >> String >> Array Number
+  dataMap =
+    foldl dataMapFoldFn M.empty records
+
+  dataMapFoldFn
+    ∷ Maybe String >> Maybe String >> String >> Array Number
+    → Json
+    → Maybe String >> Maybe String >> String >> Array Number
+  dataMapFoldFn acc js =
+    case toString =<< cursorGet r.category js of
+      Nothing → acc
+      Just categoryKey →
+        let
+          mbParallel = toString =<< flip cursorGet js =<< r.parallel
+          mbDonut = toString =<< flip cursorGet js =<< r.donut
+          values = foldMap A.singleton $ toNumber =<< cursorGet r.value js
+
+          alterParallelFn
+            ∷ Maybe (Maybe String >> String >> Array Number)
+            → Maybe (Maybe String >> String >> Array Number)
+          alterParallelFn Nothing =
+            Just $ M.singleton mbDonut $ M.singleton categoryKey values
+          alterParallelFn (Just donut) =
+            Just $ M.alter alterDonutFn mbDonut donut
+
+          alterDonutFn
+            ∷ Maybe (String >> Array Number)
+            → Maybe (String >> Array Number)
+          alterDonutFn Nothing =
+            Just $ M.singleton categoryKey values
+          alterDonutFn (Just category) =
+            Just $ M.alter alterCategoryFn categoryKey category
+
+          alterCategoryFn
+            ∷ Maybe (Array Number)
+            → Maybe (Array Number)
+          alterCategoryFn Nothing = Just values
+          alterCategoryFn (Just arr) = Just $ arr ⊕ values
+        in
+          M.alter alterParallelFn mbParallel acc
+
+
+  rawSeries ∷ Array OnePieSeries
+  rawSeries =
+    foldMap mkOnePieSeries $ M.toList dataMap
+
+  mkOnePieSeries
+    ∷ Maybe String × (Maybe String >> String >> Array Number)
+    → Array OnePieSeries
+  mkOnePieSeries (name × donutSeries) =
+    [{ name
+     , x: Nothing
+     , y: Nothing
+     , radius: Nothing
+     , series: foldMap mkDonutSeries $ M.toList donutSeries
+     }]
+
+  mkDonutSeries
+    ∷ Maybe String × (String >> Array Number)
+    → Array DonutSeries
+  mkDonutSeries (name × items) =
+    [{ name
+     , radius: Nothing
+     , items: map (Ag.runAggregation r.valueAggregation) items
+     }]
+
+  series ∷ Array OnePieSeries
+  series = adjustPosition $ map (\x → x{series = adjustDonutRadiuses x.series}) rawSeries
+
+  adjustPosition ∷ Array OnePieSeries → Array OnePieSeries
+  adjustPosition a = a
+
+  adjustDonutRadiuses ∷ Array DonutSeries → Array DonutSeries
+  adjustDonutRadiuses a = a
+
 buildPie ∷ PieR → JArray → DSL OptionI
 buildPie r records = do
-  pure unit
+  E.tooltip E.triggerItem
+
+  E.colors colors
+
+  E.legend do
+    E.leftLeft
+    E.textStyle do
+      E.fontSize 12
+      E.fontFamily "Ubuntu, sans"
+    E.orient ET.Vertical
+    E.items $ map ET.strItem legendNames
+
+  E.series series
+
+  E.titles
+    $ traverse_ E.title titles
+
+  where
+  pieData ∷ Array OnePieSeries
+  pieData = buildPieData r records
+
+  legendNames ∷ Array String
+  legendNames =
+    A.fromFoldable
+      $ foldMap (_.series
+                 ⋙ foldMap (_.items
+                            ⋙ M.keys
+                            ⋙ Set.fromFoldable)
+                )
+        pieData
+
+  series ∷ ∀ i. DSL (pie ∷ ETP.I|i)
+  series = for_ pieData \{x, y, radius, series} →
+    for_ series \{radius, items, name} → E.pie do
+      E.buildCenter do
+        traverse_ (E.setX ∘ E.percents) x
+        traverse_ (E.setY ∘ E.percents) y
+
+      for_ radius \{start, end} →
+        E.buildRadius do
+          E.setStart $ E.percents start
+          E.setEnd $ E.percents end
+
+      for_ name E.name
+
+      E.buildItems $ for_ (M.toList $ items) \(key × value) →
+        E.addItem do
+          E.value value
+          E.name key
+
+  titles ∷ Array (DSL ETP.TitleI)
+  titles = pieData <#> \{name, x, y, radius} → do
+    for_ name E.text
+    E.textStyle do
+      E.fontFamily "Ubuntu, sans"
+      E.fontSize 12
+    traverse_ (E.top ∘ ET.Percent) y
+    traverse_ (E.left ∘ ET.Percent) x
+    E.textCenter
+    E.textBottom
