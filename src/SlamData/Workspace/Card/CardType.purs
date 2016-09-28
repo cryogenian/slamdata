00c1ed3d7d01cb66d2a6fc91af816b5d64f6309b
Maxim Zimaliev <zimaliev@gmail.com>
2016-09-28 18:26:15 +0300

    added setup chart icons

--- src/SlamData/Workspace/Card/CardType.purs
+++ src/SlamData/Workspace/Card/CardType.purs
@@ -39,11 +39,9 @@ import Halogen.HTML.Indexed as HH
 import Halogen.HTML.Properties.Indexed as HP
 
 import SlamData.Workspace.Card.CardType.ChartType
-  ( ChartType
+  ( ChartType(..)
   , printChartType
   , parseChartType
-  , chartLightIconSrc
-  , chartDarkIconSrc
   )
 
 import Test.StrongCheck.Arbitrary as SC
@@ -172,43 +170,109 @@ cardName = case _ of
 
 darkCardGlyph ∷ ∀ s f. CardType → H.HTML s f
 darkCardGlyph = case _ of
-  Ace MarkdownMode → HH.img [ HP.src "img/cardsDark/setupMarkdown.svg" ]
-  Ace SQLMode → HH.img [ HP.src "img/cardsDark/query.svg" ]
-  Search →  HH.img [ HP.src "img/cardsDark/search.svg" ]
-  ChartOptions chty → HH.img [ HP.src $ chartDarkIconSrc chty ]
-  Download → HH.img [ HP.src "img/cardsDark/showDownload.svg" ]
-  Variables → HH.img [ HP.src "img/cardsDark/setupVariables.svg" ]
-  Troubleshoot → HH.img [ HP.src "img/cardsDark/troubleshoot.svg" ]
-  Chart → HH.img [ HP.src "img/cardsDark/showChart.svg" ]
-  Markdown → HH.img [ HP.src "img/cardsDark/showMarkdown.svg" ]
-  Table → HH.img [ HP.src "img/cardsDark/table.svg" ]
-  NextAction → HH.text ""
-  Cache → HH.img [ HP.src "img/cardsDark/cache.svg" ]
-  Open → HH.img [ HP.src "img/cardsDark/open.svg" ]
-  DownloadOptions → HH.img [ HP.src "img/cardsDark/setupDownload.svg" ]
-  Draftboard → HH.img [ HP.src "img/cardsDark/draftboard.svg" ]
-  ErrorCard → HH.text ""
-  PendingCard → HH.text ""
+  Ace MarkdownMode →
+    HH.img [ HP.src "img/cardsDark/setupMarkdown.svg" ]
+  Ace SQLMode →
+    HH.img [ HP.src "img/cardsDark/query.svg" ]
+  Search →
+    HH.img [ HP.src "img/cardsDark/search.svg" ]
+  ChartOptions chty →
+    let
+      src = case chty of
+        Pie → "img/cardsDark/buildChart/pie.svg"
+        Line → "img/cardsDark/buildChart/line.svg"
+        Bar → "img/cardsDark/buildChart/bar.svg"
+        Area → "img/cardsDark/buildChart/area.svg"
+        Scatter → "img/cardsDark/buildChart/scatter.svg"
+        Radar → "img/cardsDark/buildChart/radar.svg"
+        Funnel → "img/cardsDark/buildChart/funnel.svg"
+        Graph → "img/cardsDark/buildChart/graph.svg"
+        Heatmap → "img/cardsDark/buildChart/heatmap.svg"
+        Sankey → "img/cardsDark/buildChart/sankey.svg"
+        Gauge → "img/cardsDark/buildChart/gauge.svg"
+        Boxplot → "img/cardsDark/buildChart/boxplot.svg"
+        Metric → "img/cardsDark/buildChart/metric.svg"
+        PivotTable → "img/cardsDark/buildChart/pivot-table.svg"
+    in HH.img [ HP.src src ]
+  Download →
+    HH.img [ HP.src "img/cardsDark/showDownload.svg" ]
+  Variables →
+    HH.img [ HP.src "img/cardsDark/setupVariables.svg" ]
+  Troubleshoot →
+    HH.img [ HP.src "img/cardsDark/troubleshoot.svg" ]
+  Chart →
+    HH.img [ HP.src "img/cardsDark/showChart.svg" ]
+  Markdown →
+    HH.img [ HP.src "img/cardsDark/showMarkdown.svg" ]
+  Table →
+    HH.img [ HP.src "img/cardsDark/table.svg" ]
+  NextAction →
+    HH.text ""
+  Cache →
+    HH.img [ HP.src "img/cardsDark/cache.svg" ]
+  Open →
+    HH.img [ HP.src "img/cardsDark/open.svg" ]
+  DownloadOptions →
+    HH.img [ HP.src "img/cardsDark/setupDownload.svg" ]
+  Draftboard →
+    HH.img [ HP.src "img/cardsDark/draftboard.svg" ]
+  ErrorCard →
+    HH.text ""
+  PendingCard →
+    HH.text ""
 
 lightCardGlyph ∷ ∀ s f. CardType → H.HTML s f
 lightCardGlyph = case _ of
-  Ace MarkdownMode → HH.img [ HP.src "img/cardsLight/setupMarkdown.svg" ]
-  Ace SQLMode → HH.img [ HP.src "img/cardsLight/query.svg" ]
-  Search →  HH.img [ HP.src "img/cardsLight/search.svg" ]
-  ChartOptions chty → HH.img [ HP.src $ chartLightIconSrc chty ]
-  Download → HH.img [ HP.src "img/cardsLight/showDownload.svg" ]
-  Variables → HH.img [ HP.src "img/cardsLight/setupVariables.svg" ]
-  Troubleshoot → HH.img [ HP.src "img/cardsLight/troubleshoot.svg" ]
-  Chart → HH.img [ HP.src "img/cardsLight/showChart.svg" ]
-  Markdown → HH.img [ HP.src "img/cardsLight/showMarkdown.svg" ]
-  Table → HH.img [ HP.src "img/cardsLight/table.svg" ]
-  NextAction → HH.text ""
-  Cache → HH.img [ HP.src "img/cardsLight/cache.svg" ]
-  Open → HH.img [ HP.src "img/cardsLight/open.svg" ]
-  DownloadOptions → HH.img [ HP.src "img/cardsLight/setupDownload.svg" ]
-  Draftboard → HH.img [ HP.src "img/cardsLight/draftboard.svg" ]
-  ErrorCard → HH.text ""
-  PendingCard → HH.text ""
+  Ace MarkdownMode →
+    HH.img [ HP.src "img/cardsLight/setupMarkdown.svg" ]
+  Ace SQLMode →
+    HH.img [ HP.src "img/cardsLight/query.svg" ]
+  Search →
+    HH.img [ HP.src "img/cardsLight/search.svg" ]
+  ChartOptions chty →
+    let
+      src = case chty of
+        Pie → "img/cardsLight/buildChart/pie.svg"
+        Line → "img/cardsLight/buildChart/line.svg"
+        Bar → "img/cardsLight/buildChart/bar.svg"
+        Area → "img/cardsLight/buildChart/area.svg"
+        Scatter → "img/cardsLight/buildChart/scatter.svg"
+        Radar → "img/cardsLight/buildChart/radar.svg"
+        Funnel → "img/cardsLight/buildChart/funnel.svg"
+        Graph → "img/cardsLight/buildChart/graph.svg"
+        Heatmap → "img/cardsLight/buildChart/heatmap.svg"
+        Sankey → "img/cardsLight/buildChart/sankey.svg"
+        Gauge → "img/cardsLight/buildChart/gauge.svg"
+        Boxplot → "img/cardsLight/buildChart/boxplot.svg"
+        Metric → "img/cardsLight/buildChart/metric.svg"
+        PivotTable → "img/cardsLight/buildChart/pivot-table.svg"
+    in HH.img [ HP.src src ]
+  Download →
+    HH.img [ HP.src "img/cardsLight/showDownload.svg" ]
+  Variables →
+    HH.img [ HP.src "img/cardsLight/setupVariables.svg" ]
+  Troubleshoot →
+    HH.img [ HP.src "img/cardsLight/troubleshoot.svg" ]
+  Chart →
+    HH.img [ HP.src "img/cardsLight/showChart.svg" ]
+  Markdown →
+    HH.img [ HP.src "img/cardsLight/showMarkdown.svg" ]
+  Table →
+    HH.img [ HP.src "img/cardsLight/table.svg" ]
+  NextAction →
+    HH.text ""
+  Cache →
+    HH.img [ HP.src "img/cardsLight/cache.svg" ]
+  Open →
+    HH.img [ HP.src "img/cardsLight/open.svg" ]
+  DownloadOptions →
+    HH.img [ HP.src "img/cardsLight/setupDownload.svg" ]
+  Draftboard →
+    HH.img [ HP.src "img/cardsLight/draftboard.svg" ]
+  ErrorCard →
+    HH.text ""
+  PendingCard →
+    HH.text ""
 
 cardClasses ∷ CardType → Array H.ClassName
 cardClasses = case _ of
