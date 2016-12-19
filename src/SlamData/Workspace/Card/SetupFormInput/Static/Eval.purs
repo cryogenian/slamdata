fac9095aff5980f9f67a10b7bac10d2ed6d58684
Maxim Zimaliev <zimaliev@gmail.com>
2016-12-19 21:21:32 +0300

    ...

--- src/SlamData/Workspace/Card/SetupFormInput/Static/Eval.purs
+++ src/SlamData/Workspace/Card/SetupFormInput/Static/Eval.purs
@@ -5,13 +5,12 @@ module SlamData.Workspace.Card.SetupFormInput.Static.Eval
 
 import SlamData.Prelude
 
-import Control.Monad.State (class MonadState, get, put)
+import Control.Monad.State (class MonadState)
 import Control.Monad.Throw (class MonadThrow)
 
 import Data.Array as Arr
 
 import SlamData.Quasar.Class (class QuasarDSL)
-import SlamData.Quasar.Query as QQ
 import SlamData.Workspace.Card.BuildChart.Semantics as Sem
 import SlamData.Workspace.Card.BuildChart.Common.Eval as BCE
 import SlamData.Workspace.Card.Port as Port
@@ -27,22 +26,16 @@ eval
   ⇒ Model
   → Port.TaggedResourcePort
   → m Port.Port
-eval m tr = do
-  records × axes ← BCE.analyze tr =<< get
-  put (Just (CEM.Analysis { taggedResource: tr, records, axes}))
-  case m of
-    Nothing → CEM.throw "Please select axis to show"
-    Just conf → do
-      eiSample ← QQ.sample tr.resource 0 1
-      case eiSample of
-        Left _ → CEM.throw "Empty or non-existent resource"
-        Right record →
-          case Arr.head record >>= flip Sem.getMaybeString conf.value of
-            Nothing → CEM.throw $ show conf.value <> " axis is not presented in this resource"
-            Just value →
-              pure
-                $ Port.Metric
-                $ { value
-                  , label: Nothing
-                  , taggedResource: tr
-                  }
+eval m tr =
+  BCE.buildChartEval' buildFn tr m
+  where
+  buildFn axes conf records =
+    case Arr.head records >>= flip Sem.getMaybeString conf.value of
+      Nothing → CEM.throw $ show conf.value <> " axis is not presented in this resource"
+      Just value →
+        pure
+        $ Port.Metric
+        $ { value
+          , label: Nothing
+          , taggedResource: tr
+          }
--- src/SlamData/Workspace/Card/SetupFormInput/TextLike/Eval.purs
+++ src/SlamData/Workspace/Card/SetupFormInput/TextLike/Eval.purs
@@ -12,6 +12,7 @@ import SlamData.Quasar.Class (class QuasarDSL)
 import SlamData.Workspace.Card.CardType.FormInputType (FormInputType)
 import SlamData.Workspace.Card.Port as Port
 import SlamData.Workspace.Card.Eval.Monad as CEM
+import SlamData.Workspace.Card.BuildChart.Common.Eval as BCE
 import SlamData.Workspace.Card.SetupFormInput.TextLike.Model (Model)
 
 eval
@@ -24,10 +25,11 @@ eval
   → Port.TaggedResourcePort
   → FormInputType
   → m Port.Port
-eval Nothing _ _ =
-  CEM.throw "Please select value"
-eval (Just conf) taggedResource formInputType =
-  pure
+eval m tr fit = BCE.buildChartEval' buildFn tr m
+  where
+  -- We need to store axes to display selects
+  buildFn axes conf records =
+    pure
     $ Port.SetupTextLikeFormInput
         { name: conf.name
         , taggedResource
