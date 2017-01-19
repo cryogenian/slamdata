a31e573197bad10dc235015459c8bc64fef5bbea
Maxim Zimaliev <zimaliev@gmail.com>
2017-01-19 22:09:43 +0300

    headers and crlf replaced

--- src/SlamData/Workspace/Card/Setups/FormInput/TextLike/Def.purs
+++ src/SlamData/Workspace/Card/Setups/FormInput/TextLike/Def.purs
@@ -1,3 +1,19 @@
+{-
+Copyright 2016 SlamData, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
+
 module SlamData.Workspace.Card.Setups.FormInput.TextLike.Def where
 
 import SlamData.Prelude
@@ -16,4 +32,6 @@ type TextLikeDef =
   { _State ∷ APrism' CCS.AnyCardState ST.StateP
   , _Query ∷ ∀ a. APrism' (Coproduct CardEvalQuery CCQ.AnyCardQuery a) (Q.QueryP a)
   , valueProjection ∷ Ax.Axes → Array JCursor
+  }
+rray JCursor
   }
