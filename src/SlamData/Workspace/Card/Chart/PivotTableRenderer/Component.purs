7ea82f73845e1f67cb313866e2fec73ad628f2f0
Nathan Faubion <nathan@n-son.com>
2017-02-07 20:57:39 -0600

    Update PivotTableRenderer component

--- src/SlamData/Workspace/Card/Chart/PivotTableRenderer/Component.purs
+++ src/SlamData/Workspace/Card/Chart/PivotTableRenderer/Component.purs
@@ -28,10 +28,11 @@ import Data.List (List, (:))
 import Data.List as List
 import Data.String as String
 
+import DOM.Event.Event (preventDefault)
+import DOM.Event.Types (Event)
+
 import Halogen as H
-import Halogen.Component.Utils (raise)
 import Halogen.HTML as HH
-import Halogen.HTML.Events.Handler as HEH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.Themes.Bootstrap3 as B
@@ -84,9 +85,11 @@ data Query a
   | Save (PTRM.Model → a)
   | StepPage PageStep a
   | SetCustomPage String a
-  | UpdatePage a
+  | UpdatePage Event a
   | ChangePageSize String a
-  | ModelUpdated a
+
+data Message
+  = ModelUpdated
 
 data PageStep
   = First
@@ -94,11 +97,17 @@ data PageStep
   | Next
   | Last
 
-type DSL = H.ComponentDSL State Query Slam
+type DSL = H.ComponentDSL State Query Message Slam
 type HTML = H.ComponentHTML Query
 
-comp ∷ H.Component State Query Slam
-comp = H.component { render, eval }
+component ∷ H.Component HH.HTML Query Unit Message Slam
+component =
+  H.component
+    { initialState: const initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
 
 render ∷ State → HTML
 render st =
@@ -235,7 +244,8 @@ render st =
 
   pageField currentPage customPage totalPages =
     HH.div_
-      [ submittable UpdatePage
+      [ HH.form
+          [ HE.onSubmit (HE.input UpdatePage) ]
           [ HH.text "Page"
           , HH.input
               [ HP.type_ HP.InputNumber
@@ -244,12 +254,6 @@ render st =
               ]
           , HH.text $ "of " <> show totalPages
           ]
-      ]
-
-  submittable ctr =
-    HH.form
-      [ HE.onSubmit \_ →
-          HEH.preventDefault $> Just (H.action ctr)
       ]
 
   nextButtons enabled =
@@ -307,7 +311,8 @@ eval = case _ of
   SetCustomPage page next → do
     H.modify _ { customPage = Just page }
     pure next
-  UpdatePage next → do
+  UpdatePage ev next → do
+    H.liftEff $ preventDefault ev
     st ← H.get
     for_ st.customPage \page → do
       let
@@ -324,7 +329,7 @@ eval = case _ of
       pageSize  = Int.floor (readFloat size)
     H.modify _ { pageSize = pageSize }
     for st.input (ifSimpleQuery pageQuery pageTree)
-    raise (H.action ModelUpdated)
+    H.raise ModelUpdated
     pure next
   Load model next → do
     H.modify _ { pageSize = model.pageSize }
@@ -332,8 +337,6 @@ eval = case _ of
   Save k → do
     { pageSize } ← H.get
     pure $ k { pageSize }
-  ModelUpdated next →
-    pure next
 
 ifSimpleQuery
   ∷ (Input → DSL Unit)
--- src/SlamData/Workspace/Card/Common/EvalQuery.purs
+++ src/SlamData/Workspace/Card/Common/EvalQuery.purs
@@ -16,20 +16,9 @@ limitations under the License.
 
 module SlamData.Workspace.Card.Common.EvalQuery
   ( CardEvalQuery(..)
+  , CardEvalMessage(..)
   , ModelUpdateType(..)
-  , raiseUpdatedC
-  , raiseUpdatedC'
-  , raiseUpdatedP
-  , raiseUpdatedP'
   ) where
-
-import SlamData.Prelude
-
-import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Aff.Class (class MonadAff)
-
-import Halogen as H
-import Halogen.Component.Utils (raise, raise')
 
 import SlamData.Workspace.Card.Eval.Monad (EvalState)
 import SlamData.Workspace.Card.Model (AnyCardModel)
@@ -55,9 +44,6 @@ import SlamData.Workspace.Card.Port as Port
 -- |
 -- | - `SetDimensions` is used to notify the card of the size of the deck upon
 -- |   resize and initialisation.
--- |
--- | - `ModelUpdated` is a query the card sends to itself so that the deck can
--- |   peek the query and know that the deck needs saving/evaluating.
 data CardEvalQuery a
   = Activate a
   | Deactivate a
@@ -67,8 +53,10 @@ data CardEvalQuery a
   | ReceiveInput Port.Port Port.DataMap a
   | ReceiveOutput Port.Port Port.DataMap a
   | ReceiveDimensions { width ∷ Number, height ∷ Number } a
-  | ZoomIn a
-  | ModelUpdated ModelUpdateType a
+
+data CardEvalMessage
+  = ZoomIn
+  | ModelUpdated ModelUpdateType
 
 -- | This type is used to indicate whether a model update only affects the
 -- | internal state of the card (and therefore only requires saving), or whether
@@ -78,39 +66,3 @@ data ModelUpdateType
   = StateOnlyUpdate
   | EvalModelUpdate
   | EvalStateUpdate EvalState
-
--- | Raises a `ModelUpdateType` self-query for a card that is a standalone
--- | component.
-raiseUpdatedC
-  ∷ ∀ s g eff
-  . MonadAff (avar ∷ AVAR | eff) g
-  ⇒ ModelUpdateType
-  → H.ComponentDSL s CardEvalQuery g Unit
-raiseUpdatedC updateType = raise $ H.action $ ModelUpdated updateType
-
--- | Raises a `ModelUpdateType` self-query for a card that is a standalone
--- | component with an expanded query algebra.
-raiseUpdatedC'
-  ∷ ∀ f s g eff
-  . MonadAff (avar ∷ AVAR | eff) g
-  ⇒ ModelUpdateType
-  → H.ComponentDSL s (CardEvalQuery ⨁ f) g Unit
-raiseUpdatedC' updateType = raise $ left $ H.action $ ModelUpdated updateType
-
--- | Raises a `ModelUpdateType` self-query for a card that is a parent
--- | component.
-raiseUpdatedP
-  ∷ ∀ s s' f' p g eff
-  . MonadAff (avar ∷ AVAR | eff) g
-  ⇒ ModelUpdateType
-  → H.ParentDSL s s' CardEvalQuery f' g p Unit
-raiseUpdatedP updateType = raise' $ H.action $ ModelUpdated updateType
-
--- | Raises a `ModelUpdateType` self-query for a card that is a parent
--- | component with an expanded query algebra.
-raiseUpdatedP'
-  ∷ ∀ s s' f f' p g eff
-  . MonadAff (avar ∷ AVAR | eff) g
-  ⇒ ModelUpdateType
-  → H.ParentDSL s s' (CardEvalQuery ⨁ f) f' g p Unit
-raiseUpdatedP' updateType = raise' $ left $ H.action $ ModelUpdated updateType
