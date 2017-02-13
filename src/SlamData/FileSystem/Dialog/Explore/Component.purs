6f09e3b8d3a7a367e0bb4b2bd7c7d4434bb67124
Gary Burgess <gary.burgess@gmail.com>
2017-02-13 23:06:26 +0000

    Explore dialog

--- src/SlamData/FileSystem/Dialog/Explore/Component.purs
+++ src/SlamData/FileSystem/Dialog/Explore/Component.purs
@@ -18,7 +18,6 @@ module SlamData.FileSystem.Dialog.Explore.Component where
 
 import SlamData.Prelude
 
-import Data.Lens (Lens', lens, (.~))
 import Data.Path.Pathy as Pt
 
 import Halogen as H
@@ -35,7 +34,6 @@ import SlamData.Config as Config
 
 import Utils.Path as UP
 
-
 type State =
   { filePath ∷ UP.FilePath
   , workspaceName ∷ String
@@ -47,21 +45,19 @@ initialState fp =
   , workspaceName: Config.newWorkspaceName
   }
 
-
-_filePath ∷ ∀ a r. Lens' {filePath ∷ a |r} a
-_filePath = lens (_.filePath) (_{filePath = _})
-
-_workspaceName ∷ ∀ a r. Lens' {workspaceName ∷ a|r} a
-_workspaceName = lens (_.workspaceName) (_{workspaceName = _})
-
-
 data Query a
   = Explore UP.FilePath String a
   | NameTyped String a
   | Dismiss a
 
-comp ∷ H.Component State Query Slam
-comp = H.component { render, eval }
+comp ∷ H.Component HH.HTML Query UP.FilePath Void Slam
+comp =
+  H.component
+    { initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
 
 render ∷ State → H.ComponentHTML Query
 render state =
@@ -69,18 +65,21 @@ render state =
     [ modalHeader "Explore file"
     , modalBody
       $ HH.form_
-          [ HH.h4_ [ HH.text
-                       $ "Create a new workspace in "
-                       ⊕ directory
-                       ⊕ " to explore "
-                       ⊕ fileName ]
+          [ HH.h4_
+              [ HH.text
+                  $ "Create a new workspace in "
+                  ⊕ directory
+                  ⊕ " to explore "
+                  ⊕ fileName
+              ]
           , formGroup
-              [ HH.input [ HP.classes [ B.formControl ]
-                         , HP.value state.workspaceName
-                         , HP.placeholder "New workspace name"
-                         , ARIA.label "New workspace name"
-                         , HE.onValueInput (HE.input NameTyped)
-                         ]
+              [ HH.input
+                  [ HP.classes [ B.formControl ]
+                  , HP.value state.workspaceName
+                  , HP.placeholder "New workspace name"
+                  , ARIA.label "New workspace name"
+                  , HE.onValueInput (HE.input NameTyped)
+                  ]
               ]
           ]
     , modalFooter
@@ -105,7 +104,10 @@ render state =
     directory = Pt.printPath $ UP.getDir anyPath
     fileName = UP.getNameStr anyPath
 
-eval ∷ Query ~> (H.ComponentDSL State Query Slam)
-eval (Dismiss next) = pure next
-eval (Explore res name next) = pure next
-eval (NameTyped name next) = H.modify (_workspaceName .~ name) $> next
+eval ∷ Query ~> H.ComponentDSL State Query Void Slam
+eval = case _ of
+  Dismiss next → pure next
+  Explore res name next → pure next
+  NameTyped name next → do
+    H.modify (_ { workspaceName = name })
+    pure next
