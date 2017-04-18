c87dde6fc33d7dcf066618d34a11b62b958dfd11
Maxim Zimaliev <zimaliev@gmail.com>
2017-04-18 17:43:57 +0300

    scatter ordinate and size non configurable

--- src/SlamData/Workspace/Card/Setups/DimensionMap/Defaults.purs
+++ src/SlamData/Workspace/Card/Setups/DimensionMap/Defaults.purs
@@ -189,14 +189,14 @@ statics =
       , deselectable: true
       }
   , Pr._scatterOrdinate ×
-      { dimension: D.projectionWithAggregation $ Just Ag.Sum
+      { dimension: jcursorProjection
       , label: "Y-axis"
       , value: "Choose y-axis"
       , select: "Choose y-axis"
       , deselectable: true
       }
   , Pr._scatterSize ×
-      { dimension: D.projectionWithAggregation $ Just Ag.Sum
+      { dimension: jcursorProjection
       , label: "Size"
       , value: "Choose size"
       , select: "Choose size"
@@ -219,4 +219,6 @@ dynamicMeasure prj =
 isFlat ∷ T.Projection → Boolean
 isFlat prj = case T.idReflection ^. T.unpackProjection prj of
   Just "flatValue" → true
+  Just "scatterSize" → true
+  Just "scatterOrdinate" → true
   _ → false
