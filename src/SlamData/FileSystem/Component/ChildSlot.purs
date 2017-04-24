bd9a07c8cdba0f75f7eea3b2e9dec016b769bdc2
Nathan Faubion <nathan@n-son.com>
2017-04-24 17:45:28 -0500

    Fix FS child slots

--- src/SlamData/FileSystem/Component/ChildSlot.purs
+++ src/SlamData/FileSystem/Component/ChildSlot.purs
@@ -20,7 +20,6 @@ import SlamData.Prelude
 
 import Halogen.Component.ChildPath as CP
 
-import SlamData.FileSystem.Breadcrumbs.Component as Breadcrumbs
 import SlamData.FileSystem.Dialog.Component as Dialog
 import SlamData.FileSystem.Listing.Component as Listing
 import SlamData.FileSystem.Search.Component as Search
@@ -30,7 +29,6 @@ import SlamData.Notification.Component as Notify
 type ChildQuery
   = Listing.Query
   ⨁ Search.Query
-  ⨁ Breadcrumbs.Query
   ⨁ Dialog.Query
   ⨁ Header.Query
   ⨁ Notify.Query
@@ -38,7 +36,6 @@ type ChildQuery
 
 type ChildSlot
   = Unit
-  ⊹ Unit
   ⊹ Unit
   ⊹ Unit
   ⊹ Unit
@@ -57,26 +54,20 @@ cpSearch
       Unit ChildSlot
 cpSearch = CP.cp2
 
-cpBreadcrumbs
-  ∷ CP.ChildPath
-      Breadcrumbs.Query ChildQuery
-      Unit ChildSlot
-cpBreadcrumbs = CP.cp3
-
 cpDialog
   ∷ CP.ChildPath
       Dialog.Query ChildQuery
       Unit ChildSlot
-cpDialog = CP.cp4
+cpDialog = CP.cp3
 
 cpHeader
   ∷ CP.ChildPath
       Header.Query ChildQuery
       Unit ChildSlot
-cpHeader = CP.cp5
+cpHeader = CP.cp4
 
 cpNotify
   ∷ CP.ChildPath
       Notify.Query ChildQuery
       Unit ChildSlot
-cpNotify = CP.cp6
+cpNotify = CP.cp5
