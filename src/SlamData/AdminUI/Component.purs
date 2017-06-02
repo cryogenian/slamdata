ab799e3ba90d9c00fbfcfb2c52a1a2fdc5a30540
kRITZCREEK <christoph.hegemann1337@gmail.com>
2017-06-02 15:28:23 +0200

    implements MySettings form

--- src/SlamData/AdminUI/Component.purs
+++ src/SlamData/AdminUI/Component.purs
@@ -70,9 +70,18 @@ type State =
   , formState ∷ { mySettings ∷ MySettingsState, database ∷ DatabaseState }
   }
 
-newtype MySettingsState = MySettingsState { homeDirectory ∷ String, isolateArtifacts ∷ Boolean }
+newtype MySettingsState = MySettingsState
+  { homeDirectory ∷ String
+  , isolateArtifacts ∷ Boolean
+  , isolateArtifactsDirectory ∷ String
+  }
 defaultMySettingsState ∷ MySettingsState
-defaultMySettingsState = MySettingsState { homeDirectory: "", isolateArtifacts: false }
+defaultMySettingsState =
+  MySettingsState
+    { homeDirectory: ""
+    , isolateArtifacts: false
+    , isolateArtifactsDirectory: ""
+    }
 
 -- TODO(Christoph): Do properly
 type PostgresCon = String
@@ -120,22 +129,25 @@ render state =
 tabHeader ∷ TabIndex → HTML
 tabHeader active =
   HH.ul
-    [ HP.class_ $ H.ClassName "sd-admin-ui__tabs"]
+    [ HP.class_ $ H.ClassName "tabs"]
     $ Array.fromFoldable
     $ allTabs <#> \t →
       HH.li
         (fold
           [ pure $ HE.onClick $ HE.input_ $ SetActive t
-          , guard (t == active) $> HP.class_ (H.ClassName "sd-admin-ui__tabs-selected")
+          , guard (t == active) $> HP.class_ (H.ClassName "active-tab")
           ])
         [ HH.span_ [ HH.text (tabTitle t) ] ]
 
 tabBody ∷ State → HTML
 tabBody state =
   HH.form
-    [HP.class_ $ HH.ClassName "sd-admin-ui__form-body"]
+    [HP.class_ $ HH.ClassName "tab-body"]
     case state.active of
-      MySettings → renderMySettingsForm state.formState.mySettings
+      MySettings →
+        pure $ HH.div
+          [ HP.class_ (HH.ClassName "my-settings") ]
+          (renderMySettingsForm state.formState.mySettings)
       _ → [HH.text "Not implemented"]
       -- Database → ?x
       -- Server → ?x
@@ -143,27 +155,63 @@ tabBody state =
       -- Users → ?x
       -- Group → ?x
 
+-- TODO(Christoph): Talk to Kyle
+themes ∷ Array String
+themes = ["Dark", "Light"]
+
 renderMySettingsForm ∷ MySettingsState → Array HTML
 renderMySettingsForm (MySettingsState state) =
-    [ HH.div
-        [ HP.classes [ HH.ClassName "form-group" ] ]
-        [ HH.label
-            [ HP.for "HomeDirectory" ]
-            [ HH.text "Home directory" ]
+    [ HH.fieldset
+        [ HP.class_ (HH.ClassName "home-directory") ]
+        [ HH.legend_ [ HH.text "Location of my home directory in the SlamData file system:" ]
         , HH.input
             [ HP.classes [ HH.ClassName "form-control" ]
             , HP.id_ "HomeDirectory"
             , HP.value state.homeDirectory
             ]
-        ]
-    , HH.div
-        [ HP.classes [ HH.ClassName "checkbox" ] ]
-        [ HH.label_
-            [ HH.input
-                [ HP.value state.homeDirectory
-                , HP.type_ HP.InputCheckbox
+        , HH.div
+            [ HP.classes [ HH.ClassName "form-group" ] ]
+            [ HH.div
+                [ HP.classes [ HH.ClassName "checkbox" ] ]
+                [ HH.label_
+                    [ HH.input
+                        [ HP.checked state.isolateArtifacts
+                        , HP.type_ HP.InputCheckbox
+                        ]
+                    , HH.text "Isolate SlamData artifacts to a specific location in the SlamData file system"
+                    ]
                 ]
-            , HH.text "Isolate SlamData artifacts to a specific location in the SlamData file system"
+            , HH.input
+                [ HP.classes [ HH.ClassName "form-control" ]
+                , HP.id_ "IsolateLocation"
+                , HP.disabled (not state.isolateArtifacts)
+                , HP.value state.isolateArtifactsDirectory
+                ]
+            , HH.p_
+                [ HH.text $
+                  fold
+                  [ "If you choose this option, while you can still virtually locate decks anywhere inside the file system, "
+                  , "they will always be physically stored in the above location. This allows you to keep production systems "
+                  , "free of SlamData artifacts, while still centrally locating and backing them up."
+                  ]
+                ]
+            ]
+        ]
+    , HH.fieldset
+        [ HP.class_ (HH.ClassName "themes") ]
+        [ HH.legend_ [ HH.text "Default theme for new decks:" ]
+        , HH.div
+            [ HP.class_ (HH.ClassName "theme-pickers") ]
+            [ HH.select
+                [ HP.classes [ HH.ClassName "form-control" ]
+                , HP.id_ "ThemeSelection"
+                ]
+                (themes <#> \t → HH.option_ [HH.text t])
+            , HH.select
+                [ HP.classes [ HH.ClassName "form-control" ]
+                , HP.id_ "ThemeSpacing"
+                ]
+                (themes <#> \t → HH.option_ [HH.text t])
             ]
         ]
     ]
