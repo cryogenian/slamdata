2c9876b92298885ac36b0a765c092a7da8478e14
Gary Burgess <gary.burgess@gmail.com>
2017-06-07 15:17:01 +0100

    Update mount code for changes in purescript-quasar
    Also start introducing `V` error handling so we can annotate specific
    fields in the future. At the moment it mashes back down into a `Either
    String` to preserve the old behaviour.

--- src/SlamData/FileSystem/Dialog/Mount/SparkHDFS/Component.purs
+++ src/SlamData/FileSystem/Dialog/Mount/SparkHDFS/Component.purs
@@ -18,33 +18,31 @@ module SlamData.FileSystem.Dialog.Mount.SparkHDFS.Component
   ( comp
   , Query
   , module SlamData.FileSystem.Dialog.Mount.Common.SettingsQuery
-  , module MCS
+  , module S
   ) where
 
 import SlamData.Prelude
 
 import Data.Array as Array
 import Data.Path.Pathy (dir, (</>))
-
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-
 import Quasar.Mount as QM
-
-import SlamData.Monad (Slam)
 import SlamData.FileSystem.Dialog.Mount.Common.Render as MCR
 import SlamData.FileSystem.Dialog.Mount.Common.SettingsQuery (SettingsQuery(..), SettingsMessage(..))
-import SlamData.FileSystem.Dialog.Mount.SparkHDFS.Component.State as MCS
+import SlamData.FileSystem.Dialog.Mount.Common.State as MCS
+import SlamData.FileSystem.Dialog.Mount.SparkHDFS.Component.State as S
 import SlamData.FileSystem.Resource (Mount(..))
-import SlamData.Quasar.Mount as API
+import SlamData.Monad (Slam)
 import SlamData.Quasar.Error as QE
+import SlamData.Quasar.Mount as API
 import SlamData.Render.ClassName as CN
 
-type Query = SettingsQuery MCS.State
+type Query = SettingsQuery S.State
 
-comp ∷ MCS.State → H.Component HH.HTML Query Unit SettingsMessage Slam
+comp ∷ S.State → H.Component HH.HTML Query Unit SettingsMessage Slam
 comp initialState =
   H.component
     { initialState: const initialState
@@ -53,16 +51,16 @@ comp initialState =
     , receiver: const Nothing
     }
 
-render ∷ MCS.State → H.ComponentHTML Query
+render ∷ S.State → H.ComponentHTML Query
 render state =
   HH.div
     [ HP.class_ CN.mountSpark ]
-    [ MCR.section "Spark Server" [ MCR.host state MCS._sparkHost ]
-    , MCR.section "HDFS Server" [ MCR.host state MCS._hdfsHost ]
+    [ MCR.section "Spark Server" [ MCR.host state S._sparkHost ]
+    , MCR.section "HDFS Server" [ MCR.host state S._hdfsHost ]
     , MCR.section "Root"
         [ HH.div
             [ HP.class_ CN.mountPath ]
-            [ MCR.label "Path" [ MCR.input state MCS._path [] ] ]
+            [ MCR.label "Path" [ MCR.input state S._path [] ] ]
         ]
     , MCR.section "Advanced Settings"
         [ MCR.propListTable (Array.mapWithIndex renderPropRow (state.props <> [ "" × "" ])) ]
@@ -75,8 +73,8 @@ render state =
         (guard (key ≠ "") $> "")
           <> [ key ]
           <> state.availableProps
-      updateFnKey = MCS.updatePropAt ix ∘ flip Tuple value
-      updateFnVal = MCS.updatePropAt ix ∘ Tuple key
+      updateFnKey = S.updatePropAt ix ∘ flip Tuple value
+      updateFnVal = S.updatePropAt ix ∘ Tuple key
       classes = [ CN.formControl ]
     in
     HH.tr_
@@ -98,18 +96,18 @@ render state =
           ]
       ]
 
-eval ∷ Query ~> H.ComponentDSL MCS.State Query SettingsMessage Slam
+eval ∷ Query ~> H.ComponentDSL S.State Query SettingsMessage Slam
 eval = case _ of
   ModifyState f next → do
     H.modify f
     H.raise Modified
     pure next
   Validate k →
-    k <<< either Just (const Nothing) <<< MCS.toConfig <$> H.get
+    k <<< either Just (const Nothing) <<< MCS.vToE <<< S.toConfig <$> H.get
   Submit parent name k →
     k <$> runExceptT do
       st ← lift H.get
-      config ← except $ lmap QE.msgToQError $ MCS.toConfig st
+      config ← except $ lmap QE.msgToQError $ MCS.vToE $ S.toConfig st
       let path = parent </> dir name
       ExceptT $ API.saveMount (Left path) (QM.SparkHDFSConfig config)
       pure $ Database path
