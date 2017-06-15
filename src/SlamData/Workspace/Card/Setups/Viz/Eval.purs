bfd5496e4789481c908d07227172927bf2c0985e
Maxim Zimaliev <zimaliev@gmail.com>
2017-06-15 22:06:51 +0300

    ...

--- src/SlamData/Workspace/Card/Setups/Viz/Eval.purs
+++ src/SlamData/Workspace/Card/Setups/Viz/Eval.purs
@@ -5,15 +5,32 @@ module SlamData.Workspace.Card.Setups.Viz.Eval
 
 import SlamData.Prelude
 
+import Control.Monad.State (class MonadState, get, put)
+import Control.Monad.Writer.Class (class MonadTell)
+
+import Data.StrMap as SM
+
 import SlamData.Workspace.Card.Port as Port
 import SlamData.Workspace.Card.Setups.Axis (Axes)
 import SlamData.Workspace.Card.Setups.Viz.Model (Model, ModelR)
 import SlamData.Workspace.Card.Setups.Chart.Common as SCC
 import SlamData.Workspace.Card.Setups.Common.Eval as BCE
+import SlamData.Workspace.Card.Port as Port
+import SlamData.Workspace.Card.Error as CE
+import SlamData.Workspace.Card.Eval.Monad as CEM
+import SlamData.Quasar.Class (class QuasarDSL)
 
-eval ∷ ∀ m. BCE.ChartSetupEval ModelR m
-eval = traceAny "trololo" \_ → BCE.chartSetupEval (SCC.buildBasicSql (const mempty) $ const Nothing) buildPort
-
-buildPort ∷ ModelR → Axes → Port.Port
-buildPort m axes =
-  Port.ResourceKey "foo"
+eval
+  ∷ ∀ m
+  . MonadState CEM.CardState m
+  ⇒ MonadThrow CE.CardError m
+  ⇒ MonadAsk CEM.CardEnv m
+  ⇒ MonadTell CEM.CardLog m
+  ⇒ QuasarDSL m
+  ⇒ Model
+  → Port.Resource
+  → m Port.Out
+eval m resource = do
+  records × axes ← BCE.analyze resource =<< get
+  put $ Just $ CEM.Analysis { resource, records, axes }
+  pure $ Port.ResourceKey "foo" × SM.empty
