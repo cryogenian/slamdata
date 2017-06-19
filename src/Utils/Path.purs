443e950ff00f25c3da6a98942d2d6a9b1bd99160
kRITZCREEK <christoph.hegemann1337@gmail.com>
2017-06-19 16:50:58 +0200

    move toPathList into Utils.Path

--- src/Utils/Path.purs
+++ src/Utils/Path.purs
@@ -22,26 +22,22 @@ module Utils.Path
 import Prelude
 
 import Control.Alt ((<|>))
-
 import Data.Array (intersect, null, (:))
 import Data.Char as Ch
 import Data.Either (Either(..), either, fromRight)
+import Data.List as L
 import Data.Maybe (Maybe(..), maybe, fromMaybe)
 import Data.Path.Pathy (Sandboxed, Unsandboxed, Abs, Path, File, Rel, Dir, DirName(..), FileName(..), peel, rootDir, (</>), file, canonicalize, printPath, parseAbsDir, parseAbsFile, dir, relativeTo, renameDir)
 import Data.Path.Pathy as P
 import Data.String as S
 import Data.String.Regex as Rgx
 import Data.String.Regex.Flags as RXF
-import Data.Tuple (snd, fst)
-
+import Data.Tuple (Tuple(..), fst, snd)
+import Data.Unfoldable (unfoldr)
 import Global as Global
-
-import Quasar.Types (AnyPath, DirPath, FilePath)
-
 import Partial.Unsafe (unsafePartial)
-
+import Quasar.Types (AnyPath, DirPath, FilePath)
 import SlamData.Config as Config
-
 import Text.SlamSearch.Parser.Tokens (keyChars)
 
 infixl 6 renameDirExt as <./>
@@ -158,3 +154,10 @@ getDir ap = either getDir' getDir' ap
 
 rootFile ∷ FilePath
 rootFile = rootDir </> file ""
+
+toPathList ∷ AnyPath → L.List AnyPath
+toPathList res =
+  (unfoldr \r → Tuple r <$> either go go r) res `L.snoc` Left rootDir
+  where
+  go ∷ ∀ b. Path Abs b Sandboxed → Maybe AnyPath
+  go = map (Left <<< fst) <<< peel
