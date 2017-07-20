cd42e28deb16e6c3c0f8cc18da2b5db1a42b590e
Maxim Zimaliev <zimaliev@gmail.com>
2017-07-20 17:59:10 +0300

    typo

--- src/SlamData/Workspace/Card/Setups/Auxiliary/Gauge.purs
+++ src/SlamData/Workspace/Card/Setups/Auxiliary/Gauge.purs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 
-module SlamData.Workspace.Card.Setups.Auxiliary.Graph where
+module SlamData.Workspace.Card.Setups.Auxiliary.Gauge where
 
 import SlamData.Prelude hiding (case_)
 
